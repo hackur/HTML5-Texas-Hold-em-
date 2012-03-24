c90766e76246f349cf403d8fd25fb8a4fe5d2c2b
vencent <vencent@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-03-24 13:58:13 +0000

    git-svn-id: http://10.0.0.3/svn/poker@17 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- dealer.py
+++ dealer.py
@@ -1,41 +1,102 @@
 import pika
 import sys
 from optparse import OptionParser
+import random
+from sqlalchemy.orm import sessionmaker,relationship, backref
+from database import DatabaseConnection,User,Room,MessageQueue
+try:
+    import cpickle as pickle
+except:
+    import pickle
+
+
+class Seat(object):
+    (SEAT_EMPTY,SEAT_WAITING,SEAT_PLAYING) = (0,1,2)
+
+    def __init__(self):
+        self._user = None
+        self._cards = None
+        self._inAmount = 0
+        self._status = Seat.SEAT_EMPTY
+        pass
+
+
+class Cards(object):
+    def __init__(self):
+        self._cards = []
+        self._cardCount = 52
+        for i in xrange(4):
+            self._cards.append(range(1,14))
+
+    def next(self):
+        ram = random.randint(0,self._cardCount)
+        for i in xrange(4):
+            if ram >= len(self._cards[i]):
+                ram -= len(self._cards[i])
+            else:
+                self._cardCount -= 1
+                return self._cards[i][ram]
+
 
 
 
 class Dealer(object):
-	def __init__(self,queue,exchange):
-		self.queue	= queue
-		self.exchange	= exchange
+    def __init__(self,queue,exchange,num_of_seats=9,blind=100,host='localhost'):
+        self.queue	= queue
+        self.exchange	= exchange
+        self.seats = {}
+        for x in xrange(num_of_seats):
+            self.seats[x] = Seat()
 
-	def start(self):
-		self.connection	= pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-		self.channel	= self.connection.channel()
-		self.channel.exchange_declare(exchange		= self.exchange,
-						type		= 'direct',
-						auto_delete	= True,
-						durable		= False)
-		self.channel.queue_declare(queue	= self.queue,
-					auto_delete	= True,
-					durable		= False,
-					exclusive	= False)
 
-		self.channel.queue_bind(exchange	= self.exchange,
-					queue		= self.queue,
-					routing_key	= 'dealer')
-		self.channel.basic_consume(self.on_message, self.queue, no_ack=True)
-		self.channel.start_consuming()
+    def start(self):
+        self.connection	= pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel	= self.connection.channel()
+        self.channel.exchange_declare(exchange		= self.exchange,
+                type		= 'direct',
+                auto_delete	= True,
+                durable		= False)
+        self.channel.queue_declare(queue	= self.queue,
+                auto_delete	= True,
+                durable		= False,
+                exclusive	= False)
 
-	def on_message(self, channel, method, header, body):
-		print "receive:" + body
-		#do something
-		message = "message received, thanks!"
-		self.channel.basic_publish(exchange = self.exchange,
-					routing_key='player*',
-					body=message)
-	def close(self):
-		self.connection.close()
+        self.channel.queue_bind(exchange	= self.exchange,
+                queue		= self.queue,
+                routing_key	= 'dealer')
+        self.channel.basic_consume(self.on_message, self.queue, no_ack=True)
+        self.channel.start_consuming()
+
+    def cmd_sit(self,args):
+        """ User clicked Sit Down"""
+        dbConnection	= DatabaseConnection()
+        dbConnection.start_session()
+        routing_key = args['source']
+        user		= dbConnection.query(User).filter_by(id=args['user_id']).one()
+        print user
+        self.channel.basic_publish(exchange = self.exchange,
+                routing_key=routing_key,
+                body="Haven't implemented")
+
+    def cmd_init(self,args):
+        """ RETURN THE ROOM's status """
+        routing_key = args['source']
+        self.channel.basic_publish(exchange = self.exchange,
+                routing_key=routing_key,
+                body="Haven't implemented")
+
+
+    def on_message(self, channel, method, header, body):
+        print "receive:" + body
+        #do something
+        message = "message received, thanks!"
+        obj = pickle.loads(body)
+        print obj['method']
+        method = getattr(self,"cmd_" + obj['method'])
+        method(obj)
+
+    def close(self):
+        self.connection.close()
 
 
 if __name__ == "__main__":
@@ -44,12 +105,15 @@ if __name__ == "__main__":
     parser.add_option('-e', '--exchange', action='store', dest='exchange_id', help='name of the exchange to be assigned')
     (options, args) = parser.parse_args(sys.argv)
 
-    if(options.queue_id):
-        print "queue :" + options.queue_id
-    if(options.exchange_id):
-        print "queue :" + options.exchange_id
+    if not options.queue_id :
+        options.queue_id = "dealer_queue_1"
+
+    print "queue :" + options.queue_id
+
+    if not options.exchange_id:
+        options.exchange_id = "dealer_exchange_1"
+
+    print "queue :" + options.exchange_id
 
     dealer = Dealer(queue = options.queue_id, exchange = options.exchange_id)
-    print "YES!"
     dealer.start()
-    print "YES!@"
--- send.py
+++ send.py
@@ -1,13 +1,36 @@
 import pika
 import sys
+
+try:
+    import cpickle as pickle
+except:
+    import pickle
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.exchange_declare(exchange	= 'dealer_exchange_1',
 			type		= 'direct',
 			auto_delete     = True,
 			durable         = False)
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+channel.queue_bind(exchange='dealer_exchange_1',
+            queue=queue_name,routing_key="IAMGOD")
+
+def callback(ch, method, properties, body):
+    print " [x] %r:%r" % (method.routing_key, body,)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+channel.basic_publish(exchange='dealer_exchange_1',
+                      routing_key="dealer",
+                      body=pickle.dumps({'method':'init','source':'IAMGOD'}))
 
 channel.basic_publish(exchange='dealer_exchange_1',
-	        routing_key='dealer',
-		body='Hello World!')
-connection.close()
+                      routing_key="dealer",
+                      body=pickle.dumps({'method':'sit','source':'IAMGOD','user_id':'1'}))
+
+channel.start_consuming()
+
+
