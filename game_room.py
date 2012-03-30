4d922aa46cd99bd867a4c86eacd0c1fe689b903b
mcma <mcma@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-03-30 10:23:33 +0000

    add game_room
    git-svn-id: http://10.0.0.3/svn/poker@73 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- game_room.py
+++ game_room.py
@@ -1,4 +1,15 @@
 
+class Seat(object):
+	(SEAT_EMPTY,SEAT_WAITING,SEAT_PLAYING) = (0,1,2)
+
+	def __init__(self):
+		self._user = None
+		self._cards = None
+		self._inAmount = 0
+		self._status = Seat.SEAT_EMPTY
+		pass
+
+
 class GameRoom(object):
 	(GAME_WAIT,GAME_PLAY) = (0,1)
 	def __init__(self,room_id,owner):
@@ -8,9 +19,12 @@ class GameRoom(object):
 		self.player_list = []
 		self.waiting_list= []
 		self.audit_list = []
+		self.seats		=  []
+		for x in xrange(num_of_seats):
+			self.seats.append(Seat())
 
-	def add_player(self,player):
-		self.player_list.append(player)
+	def sit(self,player):
+		pass
 
 	def set_status(self,status):
 		self.status = status
