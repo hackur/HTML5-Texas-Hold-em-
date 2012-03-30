382a8c59feac3418d0223afcb02a35acc62bb1ae
mcma <mcma@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-03-30 12:53:58 +0000

    change authenticate.py for debugging
    git-svn-id: http://10.0.0.3/svn/poker@78 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- authenticate.py
+++ authenticate.py
@@ -2,14 +2,18 @@ import json
 from database import *
 def authenticate(functor):
 	def _authenticate(self):
-		if 'user' not in self.session:
+		#if 'user' not in self.session:
+		if False and 'user' not in self.session:
 			self.write(json.dumps({'status':'failed', 'content':'unauthenticated'}))
 			self.finish()
 			return
 		db_connection	= DatabaseConnection()
 		db_connection.start_session()
-		self.session['user'] = db_connection.query(User).filter_by(id = self.session['user'].id).one()
+		#self.session['user'] = db_connection.query(User).filter_by(id = self.session['user'].id).one()
+
+		self.session['user'] = db_connection.query(User).filter_by(username = self.get_argument('username')).one()
 		db_connection.commit_session()
 		functor(self)
 	_authenticate.__name__ = functor.__name__
 	return _authenticate
+
