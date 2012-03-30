72c17b73b237942b47ad781dbea2bb2f20e5178a
miller <miller@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-03-30 11:37:28 +0000

    git-svn-id: http://10.0.0.3/svn/poker@74 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- game_room.py
+++ game_room.py
@@ -7,12 +7,21 @@ class Seat(object):
 		self._cards = None
 		self._inAmount = 0
 		self._status = Seat.SEAT_EMPTY
+		self.combination = []
+		self.handcards = []
 		pass
+
+	def is_empty(self):
+		return self._status == SEAT_EMPTY
+
+	def sit(self, user):
+		self._user = user
+		self._status = SEAT_WAITING
 
 
 class GameRoom(object):
 	(GAME_WAIT,GAME_PLAY) = (0,1)
-	def __init__(self,room_id,owner):
+	def __init__(self,room_id,owner,num_of_seats = 9):
 		self.room_id = room_id
 		self.owner = owner
 		self.status = GAME_WAIT
@@ -23,8 +32,20 @@ class GameRoom(object):
 		for x in xrange(num_of_seats):
 			self.seats.append(Seat())
 
-	def sit(self,player):
-		pass
+	def sit(self,player,seat_no):
+		if seat_no > len(self.seats):
+			return (False, "Seat number is too large")
+		if not self.seats[seat_no].is_empty():
+			return (False, "Seat Occupied")
+		self.seats[seat_no].sit(player)
+		if len(self.seats) == 2:
+			# Timer starts to click
+		return (True, "")
+
+
+
+	def add_audit(self, player):
+		self.audit_list.append(player)
 
 	def set_status(self,status):
 		self.status = status
