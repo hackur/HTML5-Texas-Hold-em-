a0abd13b60d6da81b3c7f48f82fdacafc41c4b3c
hwakim <hwakim@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-04-02 11:29:57 +0000

    git-svn-id: http://10.0.0.3/svn/poker@115 f0252c50-933b-4dd8-9fc7-05cab60132cc

---
+++ PokerUITest/js/poker_lib.js
@@ -0,0 +1,30 @@
+
+
+(function(poker_lib, $, undefined) {
+
+	var getCard = function(suit, rank) {
+		var _suit;
+		var _rank;
+
+		switch(suit) {
+			case "S": _suit = "spade";
+				break;
+			case "H": _suit = "heart";
+				break;
+			case "D": _suit = "diamond";
+				break;
+			case "C": _suit = "club";
+				break;
+			default:
+				console.log("No such a poker.");
+				break;
+		}
+
+		_rank = rank;
+
+		return "/img/pokers/" + _suit + "/" + _rank + ".png";
+	};
+
+	poker_lib.getCard = getCard;
+
+}(window.poker_lib = window.poker_lib || {}, jQuery));
