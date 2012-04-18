7ce72645bcec63af599aa9a35234491e77151075
hwakim <hwakim@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-04-18 04:44:06 +0000

    git-svn-id: http://10.0.0.3/svn/poker@262 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- static/PerInfo/perinfo.js
+++ static/PerInfo/perinfo.js
@@ -1,1 +1,83 @@
 
+
+window.get_event_position = function(e){
+	if(window.touch_enable && e.touches){
+		if(e.touches[0]){
+			e.clientX =  e.touches[0].pageX;
+			e.clientY =  e.touches[0].pageY;
+		}
+		else{
+			e.clientX =  e.changedTouches[0].pageX;
+			e.clientY =  e.changedTouches[0].pageY;
+		}
+	}
+	return [e.clientX,e.clientY]
+}
+function decide_event(){
+	window.event_up = "touchend";
+	window.event_down = "touchstart"; 
+	window.event_move = "touchmove";
+	if(navigator.userAgent.match(/iPhone/i) ||
+	 		navigator.userAgent.match(/Android/i) ||
+			navigator.userAgent.match(/iPad/i) ||
+			navigator.userAgent.match(/iPod/i) ||
+			navigator.userAgent.match(/webOS/i) ||
+			navigator.userAgent.match(/BlackBerry/)
+	){
+		event_up = "touchend";
+		event_down = "touchstart"; 
+		event_move = "touchmove";
+		window.touch_enable = true;
+	}
+	else{
+		event_up = "mouseup"; 
+		event_down = "mousedown"; 
+		event_move = "mousemove";
+		window.touch_enable = false;
+	}
+
+	if( $.browser.webkit ) {
+			eventTransitionEnd = "webkitTransitionEnd";
+	} else if( $.browser.mozilla ) {
+			eventTransitionEnd = "transitionend";
+	} else if ($.browser.opera) {
+			eventTransitionEnd = "oTransitionEnd";
+	}
+}
+
+var info_init = function() {
+	decide_event();
+
+	window.bigframe = ["#info","#email","#market","#recharge","#friend"];
+
+	document.getElementById('bigFrame1').style.display = "block";
+	$("#info")[0].style.backgroundImage = "url(./image/left.png)";
+	$("#portrait_box").click(function() {
+		$("#change_por")[0].style.display = "block";
+	});
+	for(var i = 0; i <= bigframe.length; i++) {
+		frameControl(bigframe[i], i);
+	}
+	recharge.drag();
+};
+
+var frameControl = function(frame, i) {
+	$(frame).click(function() {	
+		document.getElementById('bigFrame' + (i+1)).style.display = "block";
+		for(var j = 0; j <= 4; j++) {
+			if( j != i) {
+				document.getElementById('bigFrame' + (j+1)).style.display = "none";
+				$(bigframe[j])[0].style.backgroundImage = "";		//not "url()"
+			}
+		}
+		if( i == 0) {
+			$(frame)[0].style.backgroundImage = "url(./image/left.png)";
+		}
+		if( i == 4) {
+			$(frame)[0].style.backgroundImage = "url(./image/right.png)";	
+		}
+		if( i > 0 && i < 4) {
+			$(frame)[0].style.backgroundImage = "url(./image/middle.png)";	
+		}
+	});
+};
