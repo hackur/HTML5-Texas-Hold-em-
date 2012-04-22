fd8d5554336e1f384230731ce97043ee75a76f02
mcma <mcma@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-04-22 03:51:24 +0000

    spliting room
    git-svn-id: http://10.0.0.3/svn/poker@316 f0252c50-933b-4dd8-9fc7-05cab60132cc

---
+++ static/room/room.js
@@ -0,0 +1,54 @@
+
+function fetchRoom(){
+	$.get(
+		"/list_room",
+		{},
+		function(data){
+			console.log(data);
+			$("#createroom").click(function(){
+				$("#setting").show();
+
+			});
+			$.each(data.rooms,function(index,room){
+				var id = room[0];
+				var blind = parseInt(room[1]);
+				var player = room[2];
+				var max_player = room[3];
+				var min_stake = room[4];
+				var max_stake = room[5];
+				var item = $('<li class="roomitem"></li>');
+				item.append($('<span class="roomid"></span>').html(id));
+				item.append($('<span class="info1"></span>').html(blind/2 + "/" + blind));
+				item.append($('<span class="info2"></span>').html(min_stake +"/" + max_stake));
+				item.append($('<span class="numplaying"></span>').html(player +"/"+max_player));
+				$("#gridcontent").append(item);
+				item.click(function(){
+					console.log(id);
+					localStorage["current_room_id"] = id;
+					window.location="./game.html";
+				});
+			});
+		},
+		'json'
+	);
+}
+function create_room(){
+	console.log($("#blind").val());
+	var blind = $("#blind").val();
+	var max_stake = $("#max_stake").val();
+	var min_stake = $("#min_stake").val();
+	var max_player = $("#max_player").val();
+	$.post(
+			"/create_room",
+			{blind:blind,max_player:max_player,
+				min_stake:min_stake,max_stake:max_stake},
+				function(data){
+					localStorage["current_room_id"]= data.room_id;
+					window.location="./game.html";
+					console.log(data);
+				},
+				'json'
+	);
+
+}
+$(fetchRoom);
