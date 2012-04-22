79e2e602803e2b55d18c323e60069906deb89f38
hwakim <hwakim@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-04-22 13:04:08 +0000

    git-svn-id: http://10.0.0.3/svn/poker@339 f0252c50-933b-4dd8-9fc7-05cab60132cc

--- static/user/perinfo.js
+++ static/user/perinfo.js
@@ -211,6 +211,10 @@ var view_friend = function(friend, i) {
 		$("#ffamily").html($("#ffamily").html() + friend.family);
 		$("#fpos").html($("#fpos").html() + friend.position);
 		$("#frank").html($("#frank").html() + friend.level);
-		$("#fpro").html($("#fpro").html() + friend.asset);			
+		$("#fpro").html($("#fpro").html() + friend.asset);
+		var url = "../." + friend.head_portrait;
+		var image = $('<img id="image2" src=' + url + ' />').appendTo($("#por_box"));
+		$("#image2").css({'width': 102, 'height': 126, 'top': 6, 'left': 8, 'position': 'absolute'});
+						
 	});
 };
