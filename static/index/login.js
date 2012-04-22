f14c23a7243583ebe3b837c92a74c4e1518a5358
mcma <mcma@f0252c50-933b-4dd8-9fc7-05cab60132cc>
2012-04-22 03:59:36 +0000

    adding index page
    git-svn-id: http://10.0.0.3/svn/poker@318 f0252c50-933b-4dd8-9fc7-05cab60132cc

---
+++ static/index/login.js
@@ -0,0 +1,49 @@
+
+
+function login_check() {
+	$('#llogin').click(function() {
+
+		$.ajax({
+			type:'post',
+		url:"/login",
+		data:{username:$('#account').val(), password:$('#password').val()},
+		success:function(data){
+			console.log(data);
+		},
+		dataTye:'json',
+		});
+		return false;
+	});
+
+	$('#lvisitor').click(function() {
+		var test_data = {};
+		var flag;
+
+		if(localStorage.getItem('username') == undefined 
+			|| localStorage.getItem('password') == undefined)
+		{
+			flag = 0;
+		}
+		else {
+			flag = 1;
+			test_data.username = localStorage.getItem('username');
+			test_data.password = localStorage.getItem('password');
+		}
+
+	$.ajax({
+		type:'post',
+		url:"/guest-login",
+		data: test_data,
+		success:function(data){
+			if(flag == 0) {	
+				result = JSON.parse(data);
+				localStorage.setItem('username', result.username);
+				localStorage.setItem('password', result.password);
+			}
+		},
+		dataTye:'json',
+	});				
+
+	return false;
+	});
+}
