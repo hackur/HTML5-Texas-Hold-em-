76ae97d0fdf7b3c7e8a201916e663a32ab21a778
mamingcao <mcma@mamingcao-Lenovo.(none)>
2012-05-08 22:50:53 +0800

    in progress

--- room_controller.py
+++ room_controller.py
@@ -120,13 +120,12 @@ class EnterRoomHandler(tornado.web.RequestHandler):
 		self.mongodb	= self.application.settings['_db']
 		msg = self.mongodb.board.find_one({"user_id":self.session["user_id"], "room_id":room._id})
 		if msg:
-			msg["message-list"] = []
+			pass
 		else:
-			msg = {"user_id":self.session["user_id"], "room_id":room.id, "message-list": []}
+			msg = {"user_id":self.session["user_id"], "room_id":room.id}
 
-		print "Saving ",msg
-		self.mongodb.board.remove({"user_id":self.session["user_id"]}) # guarantee only one exist
-		self.mongodb.board.save(msg)
+		# guarantee only one exist
+		self.mongodb.board.update({"user_id":self.session["user_id"]},msg,upsert=True)
 
 		queue				= str(user.username) + '_init'
 		exchange			= str(room.exchange)
@@ -220,7 +219,6 @@ class SitDownBoardHandler(tornado.web.RequestHandler):
 			self.finish()
 		else:
 			messages		= self.mongodb.board.find_one({"user_id":self.session["user_id"]})
-			print messages
 			room			= Room.find(_id=messages["room_id"])
 			if room is None:
 				self.finish(json.dumps({'status':'failed'}))
@@ -263,9 +261,8 @@ class SitDownBoardHandler(tornado.web.RequestHandler):
 			self.channel.close()
 			return
 		if messages['status'] == 'success':
-			board_message = self.mongodb.board.find_one({"user_id":self.session['user_id']});
-			board_message['is_sit_down'] = True
-			self.mongodb.board.save(board_message)
+			self.mongodb.board.update({"user_id":self.session['user_id']},\
+					{"$set" :{'is_sit_down':True}});
 
 		self.write(json.dumps(messages))
 		self.channel.close();
@@ -300,30 +297,37 @@ class BoardListenMessageSocketHandler(tornado.websocket.WebSocketHandler):
 		user		= self.user
 		queue		= str(user.username)  + '_broadcast'
 		exchange	= self.session['exchange']
-		messages = self.mongodb.board.find_one({"user_id":self.session["user_id"]})
-		if len(messages["message-list"]) > 0:
+
+		messages = [ msg.content for msg in BoardMessage.find_all(user_id=self.session["user_id"])]
+		if len(messages) > 0:
 			self.write_message(json.dumps(messages["message-list"]))
 
-		self.mongoSession = messages
-		self.messagesBuffer = messages["message-list"]
+		session = self.mongodb.board.find_one({"user_id":self.session["user_id"]})
+		self.mongoSession = session
+		self.messagesBuffer = messages
 		binding_keys= (self.session['public_key'], self.session['private_key'])
 		self.channel= PersistentChannel(
 				self.application.channel,
 				queue, exchange, binding_keys, self,arguments = {"x-expires":int(1800000)})
 		self.channel.add_message_action(self.message_call_back, None)
 		self.channel.connect()
+		self.lastestTimestamp = 0
 
 	def clean_matured_message(self, timestamp):
+		self.lastestTimestamp = timestamp
 		self.messagesBuffer = filter(lambda x: int(x['timestamp']) > timestamp, self.messagesBuffer)
-		#self.mongodb.board.update({"user_id": self.session["user_id"]}, board_messages)
 
 
 
 	def on_connection_close(self):
+		DatabaseConnection()[BoardMessage.table_name].remove({
+						"user_id":self.user.id,
+						'timestamp':{'$lte':self.lastestTimestamp}
+						})
+
 		user_id			= self.session['user_id']
-		mongoSession	= self.mongodb.board.find_one({"user_id": user_id})
-		mongoSession["message-list"] = self.messagesBuffer
-		self.mongodb.board.save(mongoSession)
+		for content in self.messagesBuffer:
+			BoardMessage.new(user_id,int(content['timestamp']),content)
 		self.channel.close()
 
 	def message_call_back(self, argument):
@@ -371,12 +375,21 @@ class BoardListenMessageHandler(tornado.web.RequestHandler):
 		queue		= str(user.username)  + '_broadcast'
 		exchange	= self.session['exchange']
 		self.clean_matured_message(timestamp)
-		messages = self.mongodb.board.find_one({"user_id":self.session["user_id"]})
-		if len(messages["message-list"]) > 0:
-			self.finish(json.dumps(messages["message-list"]))
+		messages = [ msg['content'] for msg in BoardMessage.get_collection().find(\
+					{
+					"user_id":self.session["user_id"],
+					"timestamp":{"$gt":timestamp}
+					},
+				sort=[("timestamp",1)]
+					)]
+
+		if len(messages) > 0:
+			print "IN DB-------------------------",timestamp
+			print messages
+			self.finish(json.dumps(messages))
 			return
 
-		binding_keys= [self.session['public_key'], self.session['private_key']]
+		binding_keys = [self.session['public_key'], self.session['private_key']]
 		if self.user.isBot:
 			binding_keys.append(self.session['bot_key'])
 
@@ -388,13 +401,17 @@ class BoardListenMessageHandler(tornado.web.RequestHandler):
 		self.channel.connect()
 
 	def clean_matured_message(self, timestamp):
-		board_messages = self.mongodb.board.find_one({"user_id":self.session["user_id"]})
-		print "========================[start]========================="
-		print board_messages
-		print "========================[ end ]========================="
-		if board_messages is not None:
-			board_messages["message-list"] = filter(lambda x: int(x['timestamp']) > timestamp, board_messages["message-list"])
-			self.mongodb.board.update({"user_id": self.session["user_id"]}, board_messages)
+		BoardMessage.get_collection().remove({
+						"user_id":self.session["user_id"],
+						'timestamp':{'$lte':timestamp}
+						})
+#		board_messages = self.mongodb.board.find_one({"user_id":self.session["user_id"]})
+#		print "========================[start]========================="
+#		print board_messages
+#		print "========================[ end ]========================="
+#		if board_messages is not None:
+#			board_messages["message-list"] = filter(lambda x: int(x['timestamp']) > timestamp, board_messages["message-list"])
+#			self.mongodb.board.update({"user_id": self.session["user_id"]}, board_messages)
 
 
 	def on_connection_close(self):
@@ -406,8 +423,12 @@ class BoardListenMessageHandler(tornado.web.RequestHandler):
 		print new_messages
 		print "------message receive end------"
 		user_id			= self.session['user_id']
-		board_messages	= self.mongodb.board.find_one({"user_id": user_id})
-		board_messages["message-list"].extend(new_messages)
-		self.mongodb.board.save(board_messages)
-		self.board_messages = board_messages["message-list"]
+		for content in new_messages:
+			BoardMessage.new(user_id,int(content['timestamp']),content)
+
+		if hasattr(self,"board_messages"):
+			self.board_messages.extend(new_messages)
+		else:
+			self.board_messages = new_messages
+
 		self.channel.close();
