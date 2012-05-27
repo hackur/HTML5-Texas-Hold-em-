92d28cde36684c010e99da7efc2d3925e38b048a
virtue <admin@Mac.local>
2012-05-27 18:07:13 +0800

    bonus, recharge update

--- static/user/recharge.js
+++ static/user/recharge.js
@@ -1,12 +1,12 @@
 (function(recharge,$){
 	//FB.init({appId: 231740453606973, status: true, cookie: true});
 	var items =[
-		{index:1, description:"1000 chips", order_info: 201201, price: "10 Credits"},
-		{index:2, description:"2000 chips", order_info: 201202, price: "19 Credits"},
-		{index:3, description:"5000 chips", order_info: 201203, price: "46 Credits"},
-		{index:4, description:"10000 chips", order_info:201204, price: "90 Credits"},
-		{index:5, description:"20000 chips", order_info:201205, price: "175 Credits"},
-		{index:6, description:"50000 chips", order_info:201206, price: "440 Credits"},
+		{index:1, description:"1000 chips", order_info: 201201, orginal_price:"10 Credits", price: "10 Credits"},
+		{index:2, description:"2000 chips", order_info: 201202, orginal_price:"20 Credits", price: "19 Credits"},
+		{index:3, description:"5000 chips", order_info: 201203, orginal_price:"50 Credits", price: "46 Credits"},
+		{index:4, description:"10000 chips", order_info:201204, orginal_price:"100 Credits", price: "90 Credits"},
+		{index:5, description:"20000 chips", order_info:201205, orginal_price:"200 Credits", price: "175 Credits"},
+		{index:6, description:"50000 chips", order_info:201206, orginal_price:"500 Credits", price: "440 Credits"},
 	];
 	var commodities = [];
 
@@ -15,16 +15,21 @@
 		entity.info= info;
 		entity.construct = function(){
 			var _itemDes = $('<div></div>');
+			var _originalPrice = $('<strike class="oprice"></strike>');
 			var _button  = $('<div class="rpurchase"></div>');
 
 			_itemDes.attr('id','convert'+entity.info.index);
 			_itemDes.html(entity.info.description);
 			
+			_originalPrice.html(entity.info.orginal_price);
+
 			_button.attr('id', "rpurchase"+entity.info.index);
 			_button.html(entity.info.price);
 			_button.bind("vclick", entity.invokeOrder);
+
 			_itemDes.appendTo($("#recharge_frame"));
 			_button.appendTo($("#recharge_frame"));
+			_originalPrice.appendTo(_itemDes);
 		};
 
 		entity.invokeOrder = function(){
